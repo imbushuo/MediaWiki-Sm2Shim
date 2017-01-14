0c66815a3acb5107d5519ee9b91696d6117c7391
Bingxing Wang <imbushuo@outlook.com>
2017-01-13 17:07:36 -0800

    Adding infrastructure for assets processing.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This project is a stub project - only used for content processing.");
+        }
+    }
+}
