25e74afb993e162f9b52a6854f7fc33c62602d6b
Bingxing Wang <imbushuo@outlook.com>
2017-01-24 04:43:13 -0800

    Improving code quality.

--- Sm2Shim.Class.php
+++ Sm2Shim.Class.php
@@ -2,25 +2,49 @@
 
 /**
 * Sm2Shim: A shim layer for FlashMP3 replacement
+* Author: Bingxing Wang, The Little Moe New LLC
 * Copyright (c) 2016 - 2017 The Little Moe New LLC. All rights reserved.
 * https://github.com/imbushuo/MediaWiki-Sm2Shim
-* Code provoded under BSD license.
+* Code provided under BSD-2-Clause license.
 */
 
 class Sm2ShimHooks {
 
     const EmptyString = '';
-    
-    public static function onParserSetup(&$parser) {
-        $parser->setHook('flashmp3', 'Sm2ShimHooks::renderLegacyFlashMp3');
+
+    /**
+     * Hook handler that configures MediaWiki parser.
+     *
+     * @param Parser $parser
+     * @return bool
+     */
+    public static function onParserSetup(Parser &$parser) {
+        $parser->setHook(Sm2ShimConstants::FlashMp3Tag, 'Sm2ShimHooks::renderLegacyFlashMp3');
+        $parser->setHook(Sm2ShimConstants::SoundManager2ButtonTag, 'Sm2ShimHooks::renderLegacySoundManager2Button');
 
         return true;
     }
 
-    public static function renderLegacyFlashMp3($input, $args, $parser, $frame) {
-
-        global $wgScriptPath;
-        $type = "1pixelout";
+    /**
+     * Method that handles legacy <flashmp3> tags.
+     *
+     * @param $input mixed between tags, or null if the tag is "closed", i.e. <sample />
+     *
+     * @param array $args Tag arguments, which are entered like HTML tag attributes;
+     * this is an associative array indexed by attribute name.
+     *
+     * @param Parser $parser The parent parser (a Parser object);
+     * more advanced extensions use this to obtain the contextual Title, parse wiki text,
+     * expand braces, register link relationships and dependencies, etc.
+     *
+     * @param PPFrame $frame The parent frame (a PPFrame object).
+     * This is used together with $parser to provide the parser with more complete
+     * information on the context in which the extension was called.
+     *
+     * @return mixed Parsed HTML content.
+     */
+    public static function renderLegacyFlashMp3(
+        $input, array $args, Parser $parser, PPFrame $frame) {
 
         // Sanity check: Did we receive non self-closed tags?
         if ($input != null) {
@@ -28,7 +52,7 @@ class Sm2ShimHooks {
             // Parse arguments without escaping characters, we will do it later
             // And files will always be the first element
             // TODO: Sanity check for the first element
-            $params = explode("|", $input);
+            $params = explode(Sm2ShimConstants::ParamsQualifier, $input);
 
             // Sanity check: Are parameters well-formed?
             if (empty($params)) return Sm2ShimHooks::EmptyString;
@@ -37,15 +61,15 @@ class Sm2ShimHooks {
             $files = $params[0];
             unset($params[0]);
 
-            $paramsParsed["files"] = $files;
-            $filesParsed = explode(",", $files);
+            $paramsParsed[Sm2ShimConstants::Sm2ShimParamTypeFiles] = $files;
+            $filesParsed = explode(Sm2ShimConstants::FilesQualifier, $files);
             if(empty($filesParsed)) return Sm2ShimHooks::EmptyString;
 
             // Sanity check: Did we retrieved more than one parameter?
             // Rest parameters will be parsed again in order to obtain key-value structure
             if (!empty($params)) {
                 foreach ($params as $param) {
-                    $keyValue = explode("=", $param);
+                    $keyValue = explode(Sm2ShimConstants::ParamValueQualifier, $param);
                     if (count($keyValue) == 2) {
                         $paramsParsed[$keyValue[0]] = $keyValue[1];
                     }
@@ -55,7 +79,8 @@ class Sm2ShimHooks {
             // LastFM support is dropped in this SM2 shim.
             // Type will be detected - any <flashmp3> tags with attribute type="lastfm" will be ignored.
             // ID support is deprecated, attempts to set ID for <flashmp3> tags will be ignored.
-            if (isset($args["type"]) && $args["type"] == "lastfm") {
+            if (isset($args[Sm2ShimConstants::FlashMp3ParamTypeId]) &&
+                $args[Sm2ShimConstants::FlashMp3ParamTypeId] == Sm2ShimConstants::FlashMp3ParamValueTypeLastFm) {
                 return Sm2ShimHooks::EmptyString;
             }
 
@@ -63,73 +88,73 @@ class Sm2ShimHooks {
             // Parse additional settings: AutoStart, Loop, Bg (Background color)
             // Going to generate HTML code.
 
-            $autoPlay = false;
-            $loop = false;
-            $baseClassName = "sm2-bar-ui";
+            $playerClasses = array(Sm2ShimConstants::Sm2ShimPlayerBaseClass);
 
-            if (isset($paramsParsed["autostart"]) && $paramsParsed["autostart"] === "yes") { 
-                $autoPlay = true;
-                $baseClassName .= " auto-play";
+            if (isset($paramsParsed[Sm2ShimConstants::FlashMp3ParamAutoPlayId]) &&
+                $paramsParsed[Sm2ShimConstants::FlashMp3ParamAutoPlayId] === "yes") {
+                array_push($playerClasses, Sm2ShimConstants::Sm2ShimAutoPlayClass);
             }
 
-            if (isset($paramsParsed["loop"]) && $paramsParsed["loop"] === "yes") {
-                $loop = true;
-                $baseClassName .= " repeat-playback";
+            if (isset($paramsParsed[Sm2ShimConstants::FlashMp3ParamLoopId]) &&
+                $paramsParsed[Sm2ShimConstants::FlashMp3ParamLoopId] === "yes") {
+                array_push($playerClasses, Sm2ShimConstants::Sm2ShimLoopPlayClass);
             }
 
-            $playlistContent = '';
+            $baseClassName = implode(Sm2ShimConstants::Space, $playerClasses);
+
+            $playlistContent = Sm2ShimConstants::EmptyString;
             $trackCount = 1;
             $locTrack = wfMessage('sm2shim-track')->plain();
 
             $parserOutput = $parser->getOutput();
 
             // Iterate all files to retrieve link
-            foreach ($filesParsed as $file) {
-                // Get address for internal files
-                $addr = '';
-                $title = '';
+            foreach ($filesParsed as $fileLocation) {
 
-                if (strpos($file, "http://") !== 0 && strpos($file, "https://") !== 0) {
-                    $title = Title::newFromText($file, NS_IMAGE);
-                    if ($title == null) {
-                        continue;
-                    }
+                $entityAddress = $fileLocation;
 
-                    $file = wfFindFile( $title );
-                    if ($file) {
-                        $addr = $file->getUrl();
-                        $title = $file->getTitle();
+                if (self::isInternalFile($fileLocation)) {
+                    // Get address for internal files
+                    $entityTitle = Title::newFromText($fileLocation, NS_IMAGE);
+                    if ($entityTitle == null) continue;
+
+                    $fileLocation = wfFindFile( $entityTitle );
+                    if ($fileLocation) {
+                        $entityAddress = $fileLocation->getUrl();
+                        $entityTitle = $fileLocation->getTitle();
                         // Add internal file dependency
-                        $parserOutput->addImage($title->getDBkey());
-                        $parserOutput->addLink($title);
+                        $parserOutput->addImage($entityTitle->getDBkey());
+                        $parserOutput->addLink($entityTitle);
                     }
+
                 } else {
-                    $addr = $file;
-                    $title = "{$locTrack} {$trackCount}";
-                    // Add external link reference
-                    $parserOutput->addExternalLink($file);
+
+                    // Generate title and add external link reference
+                    $entityTitle = "{$locTrack} {$trackCount}";
+                    $parserOutput->addExternalLink($entityAddress);
+
                 }
                 
-                $title = htmlspecialchars($title);
-                $addr = htmlspecialchars($addr);
+                $entityTitle = htmlspecialchars($entityTitle);
+                $entityAddress = htmlspecialchars($entityAddress);
                 $playlistContent .= <<<HTML
-<li><a href="{$addr}">{$title}</a></li>
+<li><a href="{$entityAddress}">{$entityTitle}</a></li>
 HTML;
 
                 $trackCount++;
             }
 
-            if ($playlistContent == '') return Sm2ShimHooks::EmptyString;
+            if ($playlistContent == Sm2ShimConstants::EmptyString) return Sm2ShimHooks::EmptyString;
 
-            $inlineBackgroundStyle = "";
-            $inlineTextColorStyle = "";
+            $inlineBackgroundStyle = Sm2ShimConstants::EmptyString;
 
             // Validate and set CSS for additional settings
-            if (isset($paramsParsed["bg"]) && $paramsParsed["bg"] != '') {
+            if (isset($paramsParsed[Sm2ShimConstants::FlashMp3ParamBackgroundId]) &&
+                $paramsParsed[Sm2ShimConstants::FlashMp3ParamBackgroundId] != Sm2ShimConstants::EmptyString) {
                 // To Lowercase and trim the magic "0x"
-                $colorRaw = strtolower($paramsParsed["bg"]);
+                $colorRaw = strtolower($paramsParsed[Sm2ShimConstants::FlashMp3ParamBackgroundId]);
                 // Make sure it starts with "0x"
-                if (strpos($colorRaw, "0x") === 0) {
+                if (strpos($colorRaw, Sm2ShimConstants::FlashMp3ParamValueBackgroundMagicHeader) === 0) {
                     // So trim the magic header
                     $colorRaw = substr($colorRaw, 2);
                     // Perform sanity check for input values
@@ -146,8 +171,8 @@ HTML;
                 $wgSm2Shim_ExternalCDNVersionControlId;
 
             if ($wgSm2Shim_UseResourceManager) {
-                $parserOutput->addModules("ext.sm2Shim");
-                $parserOutput->addModuleStyles("ext.sm2Shim");
+                $parserOutput->addModules(Sm2ShimConstants::Sm2ShimBundleId);
+                $parserOutput->addModuleStyles(Sm2ShimConstants::Sm2ShimBundleId);
             } else {
                 $cssEndpoint = "$wgSm2Shim_ExternalCDNEndpoint/css/player-ui.min.$wgSm2Shim_ExternalCDNVersionControlId.css";
                 $jsEndpoint = "$wgSm2Shim_ExternalCDNEndpoint/js/player-bundled.min.$wgSm2Shim_ExternalCDNVersionControlId.js";
@@ -156,11 +181,9 @@ HTML;
                 $parserOutput->addHeadItem($sm2ModuleHeader, $sm2ModuleHeader);
             }
 
-            // Load resources
-
             $locResPlayback = wfMessage('sm2shim-playpause')->escaped();
             $locResJsRequired = wfMessage('sm2shim-jsrequired')->escaped();
-            $locResPervious = wfMessage('sm2shim-previous')->escaped();
+            $locResPrevious = wfMessage('sm2shim-previous')->escaped();
             $locResNext = wfMessage('sm2shim-next')->escaped();
             $locResRepeat = wfMessage('sm2shim-repeat')->escaped();
             $locResMenu = wfMessage('sm2shim-menu')->escaped();
@@ -200,7 +223,7 @@ HTML;
                     </div><!-- END PLAY INDICATOR AND PLAYLIST -->
                     <div class="sm2-inline-element sm2-button-element">
                         <div class="sm2-button-bd">
-                            <a href="#prev" title="{$locResPervious}" class="sm2-inline-button sm2-icon-previous">{$locResPervious}</a>
+                            <a href="#prev" title="{$locResPrevious}" class="sm2-inline-button sm2-icon-previous">{$locResPrevious}</a>
                         </div>
                     </div>
                     <div class="sm2-inline-element sm2-button-element">
@@ -237,12 +260,53 @@ HTML;
 
             return array($output, "markerType" => 'nowiki');
         }
+
+        return Sm2ShimConstants::EmptyString;
     }
 
-    
+    /**
+     * Method that handles legacy <sm2> tags.
+     *
+     * @param $input Mixed between tags, or null if the tag is "closed", i.e. <sample />
+     *
+     * @param array $args Tag arguments, which are entered like HTML tag attributes;
+     * this is an associative array indexed by attribute name.
+     *
+     * @param Parser $parser The parent parser (a Parser object);
+     * more advanced extensions use this to obtain the contextual Title, parse wiki text,
+     * expand braces, register link relationships and dependencies, etc.
+     *
+     * @param PPFrame $frame The parent frame (a PPFrame object).
+     * This is used together with $parser to provide the parser with more complete
+     * information on the context in which the extension was called.
+     *
+     * @return mixed Parsed HTML content.
+     */
+    public static function renderLegacySoundManager2Button(
+        $input, array $args, Parser $parser, PPFrame $frame) {
+        // Currently redirects to FlashMP3 rendering.
+        return self::renderLegacyFlashMp3($input, $args, $parser, $frame);
+    }
 
-    private static function validateHexColor($colorInput) {
+    /**
+     * Method that validates color inputs.
+     *
+     * @param string $colorInput Color representation in hex string.
+     * @return bool Value indicates whether the color representation is valid or not.
+     */
+    private static function validateHexColor(string $colorInput) {
         $validationResult = preg_match('/^([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/', $colorInput);
         return !empty($validationResult);
     }
+
+    /**
+     * Method that determines whether a given file location is located locally.
+     * @param string $fileLocation File location representation.
+     * @return bool Value indicates whether a file is an internal file.
+     */
+    private static function isInternalFile(string $fileLocation) {
+        return strpos($fileLocation, Sm2ShimConstants::HttpUrlHeader) !== 0 &&
+        strpos($fileLocation, Sm2ShimConstants::HttpsUrlHeader) !== 0;
+    }
+
 }
---
+++ Sm2ShimConstants.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Sm2ShimConstants: Constants definition for SoundManager2 Shim.
+ * Author: Bingxing Wang, The Little Moe New LLC
+ * Copyright (c) 2016 - 2017 The Little Moe New LLC. All rights reserved.
+ * https://github.com/imbushuo/MediaWiki-Sm2Shim
+ * Code provided under BSD-2-Clause license.
+ */
+
+class Sm2ShimConstants {
+
+    const EmptyString = '';
+    const Space = ' ';
+    const FlashMp3Tag = 'flashmp3';
+    const SoundManager2ButtonTag = 'sm2';
+
+    const ParamsQualifier = '|';
+    const ParamValueQualifier = '=';
+    const FilesQualifier = ',';
+
+    const Sm2ShimPlayerBaseClass = 'sm2-bar-ui';
+    const Sm2ShimAutoPlayClass = 'auto-play';
+    const Sm2ShimLoopPlayClass = 'repeat-playback';
+
+    const FlashMp3ParamTypeId = 'type';
+    const FlashMp3ParamBackgroundId = 'bg';
+    const FlashMp3ParamValueTypeLastFm = 'lastfm';
+    const FlashMp3ParamValueBackgroundMagicHeader = '0x';
+    const FlashMp3ParamAutoPlayId = 'autostart';
+    const FlashMp3ParamLoopId = 'loop';
+
+    const Sm2ShimParamTypeFiles = 'files';
+    const Sm2ShimBundleId = 'ext.sm2Shim';
+
+    const HttpUrlHeader = 'http://';
+    const HttpsUrlHeader = 'https://';
+
+}
