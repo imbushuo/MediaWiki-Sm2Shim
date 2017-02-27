5eb83497843b793f2cd9810e6345f9921d9bdec5
Bingxing Wang <i@imbushuo.net>
2017-02-27 07:27:46 -0800

    Address issues in title parsing.

--- Hooks/ModernSoundManager2Hooks.php
+++ Hooks/ModernSoundManager2Hooks.php
@@ -390,7 +390,7 @@ HTML;
                         $entityAddress = $fileLocation->getUrl();
                         if ($entityAddress != "")
                         {
-                            $entityTitle = $fileLocation->getTitle()->getText();
+                            $entityTitle = $fileLocation->getTitle();
                             $entityNavigationAddress = $entityTitle->getLocalURL();
                         }
                     }
@@ -406,7 +406,7 @@ HTML;
                     $entityAddress,
                     "", /* No LRC file for legacy syntax */
                     0,  /* No LRC file for legacy syntax */
-                    $entityTitle, /* Title */
+                    $entityTitle->getText(), /* Title */
                     "", /* No manual metadata override for legacy syntax */
                     "", /* No manual metadata override for legacy syntax */
                     false,
